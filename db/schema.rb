a57242b359331f06014b8a2eba4739524d678ec6
Brian Bwori <obriann21@gmail.com>
2019-01-21 14:32:29 +0000

    adding names and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190121100538) do
+ActiveRecord::Schema.define(version: 20190121140751) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20190121100538) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
