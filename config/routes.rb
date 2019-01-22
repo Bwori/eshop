c2a0c2bf178b2ac32f6ed53d1e2a15bc6b9f0d5e
Brian Bwori <obriann21@gmail.com>
2019-01-22 08:26:47 +0000

    Createing a sellers page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   get 'pages/about'
-
   get 'pages/contact'
+  get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
   
   root 'listings#index'
  
